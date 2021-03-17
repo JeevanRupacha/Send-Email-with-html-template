e2fe9310ab52cfffa06bf9e543d2d57ef48242cd
unknown <rupachasgvn@gmail.com>
2021-03-17 21:55:00 +0545

    initial commit

--- emailService.py
+++ emailService.py
@@ -10,6 +10,7 @@ class EmailService :
     email = ''
     email_pass = ''
     sendToEmail = ''
+    html_template = ''
     msg = EmailMessage()
 
 
@@ -17,35 +18,21 @@ class EmailService :
         self.email = email
         self.email_pass = email_pass
         self.sendToEmail = sendToEmail
+
+    def set_hmtl_template(self, html_template):
+        self.html_template = html_template
     
 
     ### send birthday email 
     def sendBirthdayEmail(self):
-        email_tamplate = """
-        <!DOCTYPE html>
-        <html>
-            <body>
-                <h1 style="color:#F1239F; margin: 0px auto">Happy Birthday!</h1>
-                <h3 style="color:#F39912"> Dear Grishmin,</h3>
-                <p> I met lots of people in my life but very few like you.
-                 You are the amazing man I ever met in my life . I wish happy 3th day and many many wishes more.
-                I know its not the right time of your birthday but I gave you in advanced. Just kidding I am testing the auto email service and this is auto sended email.
-                If u get this emamil than You can give me feedback . Ok
-
-                
-                </p>
-
-                <img style="width:100%" src = "https://firebasestorage.googleapis.com/v0/b/whats-app-clone-d85bd.appspot.com/o/birthdaycard2.jpg?alt=media&token=286fb09d-6630-4902-baad-200454ec9c89"></img>
-                <h3 style="color:#F39912"> Jeevan Rupacha </h3>
-            </body>
-        </html>
-        """
+        html_tamplate = self.hmtl_template
+        
 
         self.msg['subject'] = 'Happy Birthday to You '
         self.msg['from'] = self.email
         self.msg['to'] = self.sendToEmail
         self.msg.set_content = " "
-        self.msg.add_alternative(email_tamplate, subtype = "html")
+        self.msg.add_alternative(html_tamplate, subtype = "html")
 
 
         try:
@@ -57,6 +44,3 @@ class EmailService :
             print("error occured here " + str(e))
 
 
-
-emailService = EmailService('rupachasgvn@gmail.com','rupachazevn', 'ruupachagvn@gmail.com')
-emailService.sendBirthdayEmail()
